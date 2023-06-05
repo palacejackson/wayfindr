9688974536d4c214c1ad65f366e608321e852dd0
Alice Jackson <alicejj92@gmail.com>
2023-06-05 16:58:17 +0100

    association finalised

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ApplicationRecord
+  has_many :trip_activities
   has_many :trips, through: :trip_activities
 
   validates :location, presence: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :trip_users
   has_many :trips, through: :trip_users
 end
