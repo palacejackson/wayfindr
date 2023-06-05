f2bd0388254e3923ceff0efe11cd3c137b40010e
Camillo Giugliano <camillogiugliano98000@gmail.com>
2023-06-05 16:22:47 +0100

    Associations created

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,6 @@
 class Activity < ApplicationRecord
+  has_many :trips, through: :trip_activities
+
+  validates :location, presence: true
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,5 @@
 class Trip < ApplicationRecord
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: :user_id
+  has_many :users, through: :trip_users
+  has_many :activities, through: :trip_activities
 end
