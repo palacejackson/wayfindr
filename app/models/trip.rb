16767165c492740fd2be01880bfbc099e54cbc4d
Camillo Giugliano <camillogiugliano98000@gmail.com>
2023-06-05 16:57:57 +0100

    Validations added

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,8 @@ class Trip < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
   has_many :users, through: :trip_users
   has_many :activities, through: :trip_activities
+  validates :destination, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :trips, through: :trip_users
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
 end
