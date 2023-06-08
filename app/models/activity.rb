3eafe15ccbb10ad658ee6d9d2b1c3eb77af2e119
Camillo Giugliano <camillogiugliano98000@gmail.com>
2023-06-08 12:12:26 +0100

    Added checkboxes to the form

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,7 @@ class Activity < ApplicationRecord
   has_many :trips, through: :trip_activities
 
   validates :location, presence: true
-  validates :type, presence: true
+  validates :activity_type, presence: true
   validates :name, presence: true, uniqueness: true
   validates :details, presence: true
   validates :rating, numericality: { only_integer: true, in: (0..10) }, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_144619) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_105901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "activities", force: :cascade do |t|
     t.string "location"
-    t.string "type"
     t.string "name"
     t.text "details"
     t.integer "rating"
@@ -25,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_144619) do
     t.boolean "daytime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "activity_type"
   end
 
   create_table "trip_activities", force: :cascade do |t|
