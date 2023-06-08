c047bcf73b4f0e4a74229eed0ccd2c260e42a596
Camillo Giugliano <camillogiugliano98000@gmail.com>
2023-06-08 17:09:29 +0100

    Random trip generate created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: "pages#landing"
-  get 'users/show'
-  get 'trips/show'
-  get 'trips/new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
