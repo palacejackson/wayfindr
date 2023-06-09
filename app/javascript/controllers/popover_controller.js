577da726d3b0df9e49f660ea1c051eabd617d5a2
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-06-09 10:37:10 +0100

    added popovers

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="popover"
 export default class extends Controller {
   connect() {
+    new bootstrap.Popover(this.element)
   }
 }
