7f91ebf48b565304a5ff33b9db10d61c219988c1
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-06-13 16:09:39 +0100

    past trips showing when press a button

---
+++ app/javascript/controllers/hidden_past_trips_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="hidden-past-trips"
+export default class extends Controller {
+  static targets = ["hidden", "button"]
+  connect() {
+    console.log("I am connected")
+  }
+
+  showHiddenTrips() {
+    console.log("listening");
+    console.log(this.hiddenTargets);
+    this.hiddenTargets.forEach((target) => {
+      target.classList.toggle("d-none");
+    });
+    console.dir(this.buttonTarget)
+    if (this.buttonTarget.innerHTML === "See all past trips") {
+       this.buttonTarget.innerHTML = "See less"
+       window.scrollTo(0,700);
+      } else {
+        this.buttonTarget.innerHTML = "See all past trips";
+        window.scrollTo(0,550);
+      };
+  }
+}
