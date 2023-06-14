41fab221541db56688dff4e29bf48966eac8c487
Nicole Moncrieffe <nicoletmoncrieffe@gmail.com>
2023-06-14 15:39:16 +0100

    slightly changed form

---
+++ app/javascript/controllers/alert.js
@@ -0,0 +1,12 @@
+
+// (alert) => {
+//   document.getElementById("generateBtn").addEventListener("click", function() {
+
+//     document.getElementById("alertContainer").classList.remove("d-none");
+
+
+//     setTimeout(function() {
+//       window.location.href = "your-redirect-url";
+//     }, 2000);
+//   });
+// }
