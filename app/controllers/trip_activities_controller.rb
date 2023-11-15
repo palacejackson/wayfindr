4a4cd7c046a725c8cf0c54c71c3c7dd6c0586887
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-11-15 21:15:04 +0000

    html is now being rendered without page refresh

--- app/controllers/trip_activities_controller.rb
+++ app/controllers/trip_activities_controller.rb
@@ -1,15 +1,17 @@
 class TripActivitiesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def update
-    @trip_activity = TripActivity.find(params[:id])
-    @old_dom_id = @trip_activity.id
-    @trip = @trip_activity.trip
+    response = request.body.read
+    trip_id = JSON.parse(response)
+    id = params[:id]
+    @trip_activity = TripActivity.find_by(activity_id: id, trip_id: trip_id, locked: true)
+    @trip = Trip.find_by_id(trip_id)
     @new_activity = @trip.remaining_activities.sample
     @trip_activity.update(activity: @new_activity)
+    @carousel = 0
     # TODO: put this in an if statement
-    if @trip_activity.update(activity: @new_activity)
-      respond_to do |format|
-        format.html { redirect_to trip_path(@trip) }
-      end
+    respond_to do |format|
+      format.html { render partial: 'trip_activity/trip_activity', locals: { activity: @new_activity } }
     end
   end
 end
--- app/javascript/controllers/activity_swap_controller.js
+++ app/javascript/controllers/activity_swap_controller.js
@@ -10,14 +10,28 @@ export default class extends Controller {
 
   swap(e) {
     e.preventDefault();
+    const currentUrl = new URL(window.location);
     const swappedActivityId = e.target.dataset.id
+    const url = currentUrl.pathname;
+    const trip = parseInt(url.replace(/\D/g, ''), 10);
+    const activityCard = e.target.parentElement.parentElement.parentElement.parentElement;
+    console.log(trip);
     console.log(swappedActivityId);
-    this.formTargets.forEach((form) => {
-      if (form.dataset.id == swappedActivityId) {
-        console.log(form);
-        console.log(form.action)
-        form.requestSubmit();
+    fetch(`/trip_activities/${swappedActivityId}`, {
+      method: 'PATCH',
+      body: JSON.stringify(trip),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then(response => {
+      if(response.ok) {
+        return response.text();
       }
-    })
+    }).then(html => {
+      activityCard.outerHTML = html
+    });
+
+    console.dir(activityCard)
+    // activityCard.outerHTML = html response of the rendered partial with all the new info
   }
 }
